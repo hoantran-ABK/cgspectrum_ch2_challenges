c10da1019ea155ad051fdf53ec274d637a020c05
hoantran-ABK <hoan.tran@activision.com>
2022-07-22 15:16:42 -0700

    added input validation for the 3 input values
    added while loops for input validation and adjusted print statements to be more clear

--- cgspectrum_ch2_challenges/cgspectrum_ch2_challenge1/challenge1.cpp
+++ cgspectrum_ch2_challenges/cgspectrum_ch2_challenge1/challenge1.cpp
@@ -14,11 +14,28 @@ int main() {
 	double x, y, z;
 
 	std::cout << "Please provide a number (3 left): ";
-	std::cin >> x;
-	std::cout << "Please provide a number (2 left): ";
-	std::cin >> y;
-	std::cout << "Please provide a number (1 left): ";
-	std::cin >> z;
+	//std::cin >> x;
+	while (!(std::cin >> x)) {
+		std::cout << "Invalid number, try again (3 numbers left): ";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
+	std::cout << "Please provide another number (2 left): ";
+	//std::cin >> y;
+	while (!(std::cin >> y)) {
+		std::cout << "Invalid number, try again (2 numbers left): ";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
+	std::cout << "Please provide a final number (1 left): ";
+	//std::cin >> z;
+	while (!(std::cin >> z)) {
+		std::cout << "Invalid number, try again (1 numbers left): ";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
 
 	std::cout << "Sum of the 3 numbers: " << Sum(x, y, z) << std::endl;
 	std::cout << "Average of the 3 numbers: " << Average(x, y, z) << std::endl;
